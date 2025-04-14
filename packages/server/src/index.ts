509920c063d4e97a16e0864055b733fb2959ad6b
Ben Osman <305317+benosman@users.noreply.github.com>
2025-04-14 15:02:13 +0100

    refactor(server): move types to own file

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,2 @@
-export * from './server.js';
+export { TaskServer } from './server.js';
+export * from './types.js';
---
+++ packages/server/src/types.ts
@@ -0,0 +1,41 @@
+import type { ToroTaskClient, TaskHandler, TaskOptions } from '@torotask/client';
+import type { Logger, DestinationStream, LoggerOptions } from 'pino';
+
+/** Options for configuring the TaskServer */
+export interface TaskServerOptions {
+  /**
+   * ToroTaskClient instance to use.
+   * If not provided, connection options must be supplied to create one.
+   */
+  client?: ToroTaskClient;
+  /**
+   * Options to create a ToroTaskClient if an instance is not provided.
+   * Ignored if `client` is provided.
+   */
+  clientOptions?: ConstructorParameters<typeof ToroTaskClient>[0];
+  /**
+   * Pino logger instance or options.
+   * If neither is provided, a default logger will be created.
+   */
+  logger?: Logger | DestinationStream | LoggerOptions;
+  /** Logger name to use if creating a logger */
+  loggerName?: string;
+  /**
+   * Whether the server should attach listeners to
+   * process.on('unhandledRejection') and process.on('uncaughtException').
+   * Defaults to true.
+   */
+  handleGlobalErrors?: boolean;
+  /**
+   * The root directory for resolving relative paths, typically the directory
+   * of the script creating the TaskServer instance (e.g., `__dirname`).
+   * Required if using relative paths in `loadTasksFromDirectory`.
+   */
+  rootDir?: string;
+}
+
+/** Type definition for the expected export from a task file */
+export interface TaskModule<T = unknown, R = unknown> {
+  handler: TaskHandler<T, R>;
+  options?: TaskOptions;
+}
