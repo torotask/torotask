03344faee86b26b6130d8eee1d73423afce9e25b
Ben Osman <dev@smoothify.net>
2025-10-30 18:59:47 +0000

    chore(tester): add new task group

--- apps/tester/src/tasks/index.ts
+++ apps/tester/src/tasks/index.ts
@@ -1,8 +1,10 @@
 import { defineTaskGroupRegistry } from 'torotask';
 import { exampleGroup } from './exampleGroup/index.js';
 import { differentGroup } from './differentGroup/index.js';
+import { newGroup } from './newGroup/index.js';
 
 export const taskGroups = defineTaskGroupRegistry({
   exampleGroup,
   differentGroup,
+  newGroup,
 } as const);
